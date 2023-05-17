ad9fea85e3d2e6eed5c93de636f9e6b4fa799d0a
Ravi Ghasemi <101430539+burymewithmykatana@users.noreply.github.com>
2023-05-17 21:35:13 +0330

    main page header update 0.1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
 module.exports = {
   content: ["./dist/*.html"],
   theme: {
-    extend: {},
+    screens: {
+      'mobile': '360px',
+      // => @media (min-width: 360px) { ... }
+
+      'desktop': '1440px',
+      // => @media (min-width: 1440px) { ... }
+    },
+    extend: {
+      colors:{
+        'green-primary': '#417F56',
+      }
+
+    },
   },
   plugins: [],
 }
